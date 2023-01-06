37da8549bb0db189c2653ad620315ebfce340a9e
rayhanthahir <115123525+rayhanthahir@users.noreply.github.com>
2023-01-06 18:11:53 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+const select = document.querySelectorAll(".currency");
+const btn = document.getElementById("btn");
+const num = document.getElementById("num");
+const ans = document.getElementById("ans");
+
+getPosts().then(posts => {
+  console.log(posts);
+  display(posts);
+});
+
+async function getPosts() {
+try {
+  const response = await fetch("https://api.frankfurter.app/currencies");
+  return response.json();
+} catch(error) {
+  console.log(error);
+}}
+
+function display(data) {
+  const entries = Object.entries(data);
+  for (var i = 0; i < entries.length; i++) {
+    select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+    select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+  }
+}
+
+
+btn.addEventListener("click", () => {
+  let currency1 = select[0].value;
+  let currency2 = select[1].value;
+  let value = num.value;
+
+  if (currency1 != currency2) {
+    convert(currency1, currency2, value);
+  } else {
+    alert("Cannot process, change your currency");
+  }
+});
+
+function convert(currency1, currency2, value) {
+  const host = "api.frankfurter.app";
+  fetch(
+    `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
+  )
+    .then((val) => val.json())
+    .then((val) => {
+      console.log(Object.values(val.rates)[0]);
+      ans.value = Object.values(val.rates)[0];
+    });
+}
